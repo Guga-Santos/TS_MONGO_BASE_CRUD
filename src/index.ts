c9ed9c275bf7cedf7d3220dce59db07426785b9d
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 18:29:03 -0300

    feat: connectToDatabase at index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import 'dotenv/config';
 import app from './app';
+import connectToDatabase from './models/connection';
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+connectToDatabase()
+.then(() => {
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+})
+.catch((error) => {
+  console.log('Connection with database generated an error:\r\n');
+  console.error(error);
+  console.log('\r\nServer initialization cancelled');
+  process.exit(0);
+})
+
