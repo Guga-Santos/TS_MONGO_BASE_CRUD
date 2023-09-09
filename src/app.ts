a791c08ddecba502017134d5d3c9bac5bf0898e3
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 19:02:19 -0300

    feat: import example route at app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import ExampleRouter from './routes/example.route';
 
 const app = express();
+app.use(ExampleRouter);
+app.use(errorHandler);
 
 export default app;
